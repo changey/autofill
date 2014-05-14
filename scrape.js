e2bbe1ea762fb49205d9312253e292208abd2568
Eric Chang <changey@mit.edu>
2014-05-13 23:00:15 -0700

    parsing

--- scrape.js
+++ scrape.js
@@ -6,26 +6,71 @@
 //    return $('.fr-table-heading').text();
 //  }
 //});
+var js = document.createElement("script");
+
+js.type = "text/javascript";
+js.src = "vendor/js/underscore.js";
+
+document.body.appendChild(js);
 
 pjs.addSuite({
-  url: 'http://en.wikipedia.org/wiki/List_of_towns_in_Vermont',
+  url: 'file://localhost/Users/changey/Documents/aaproject_ms/autofill/success_search.html',
   scraper: function() {
-    return $('#sortable_table_id_0 tr').slice(1).map(function() {
-      var name = $('td:nth-child(2)', this).text(),
-        county = $('td:nth-child(3)', this).text(),
-      // convert relative URLs to absolute
-        link = _pjs.toFullUrl(
-          $('td:nth-child(2) a', this).attr('href')
-        );
-      return {
-        model: "myapp.town",
-        fields: {
-          name: name,
-          county: county,
-          link: link
-        }
+    var output = [];
+    $.each($('#tblODs tr').slice(3), function() {
+      var totalDeparts,
+          totalArrives,
+          totalDuration,
+          stops = "",
+          cabin = "",
+          type = "";
+      if (this.className.indexOf("tbl-collapse") > -1) {
+        type = "multi";
+      } else {
+        type = "direct";
       }
-    }).toArray(); // don't forget .toArray() if you're using .map()
+      var flightNumber = $('td:nth-child(2)', this).text();
+        if (flightNumber === "Multiple") {
+            totalDeparts = $('td:nth-child(3)', this).text();
+            totalArrives = $('td:nth-child(4)', this).text();
+            totalDuration = $('td:nth-child(5)', this).text();
+        } else {
+          totalDeparts = $('td:nth-child(3)', this).text();
+          totalArrives = $('td:nth-child(4)', this).text();
+          totalDuration = $('td:nth-child(5)', this).text();
+          stops = $('td:nth-child(6)', this).text();
+          cabin = $('td:nth-child(6)', this).text();
+        }
+
+      var flight = {
+        type: type,
+        flightNumber: flightNumber,
+        departs: totalDeparts,
+        arrives: totalArrives,
+        duration: totalDuration,
+        stops: stops,
+        cabin: cabin
+      }
+      output.push(flight);
+    });
+    var outputJson = JSON.stringify(output, null, ' ');
+    console.log(outputJson);
+
+//    return $('#tblODs tr').slice(3).map(function() {
+//      var flightNumber = $('td:nth-child(2)', this).text(),
+//        departs = $('td:nth-child(3)', this).text(),
+//      // convert relative URLs to absolute
+//        link = _pjs.toFullUrl(
+//          $('td:nth-child(2) a', this).attr('href')
+//        );
+//      return {
+//        model: "myapp.flight",
+//        fields: {
+//          flightNumber: flightNumber,
+//          departs: departs,
+//          link: link
+//        }
+//      }
+//    }).toArray(); // don't forget .toArray() if you're using .map()
   }
 });
--
